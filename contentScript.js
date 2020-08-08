32c5ce06713e3825442763a77a7ea831bc93823d
Ernest Chang <iattempt.net@gmail.com>
2020-08-08 23:13:33 +0800

    refactor: remove unused code

--- contentScript.js
+++ contentScript.js
@@ -121,12 +121,3 @@ const debouncedMain = debounce(main);
 
 window.addEventListener("load", debouncedMain);
 observe(debouncedMain);
-
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  console.log(
-    sender.tab
-      ? "from a content script:" + sender.tab.url
-      : "from the extension"
-  );
-  if (request.greeting == "hello") sendResponse({ farewell: "goodbye" });
-});
